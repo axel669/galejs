050ee8a11601e73fd98caa959b7e44e7ba269ca8
axel669 <haryuu.hanekata@gmail.com>
2026-07-20 22:18:09 -0700

    [add] +transpiler options in rollup

--- compiler/rollup.js
+++ compiler/rollup.js
@@ -2,14 +2,27 @@ import fs from "fs-jetpack"
 
 import { transpile } from "./transpiler.js"
 
-export default {
-    name: "galejs",
-    async load(filepath) {
-        if (filepath.includes("node_modules") === true) {
-            return null
+const defaults = {
+    sourceImport: "@axel669/galejs"
+}
+export default (options) => {
+    const opts = {
+        ...defaults,
+        ...options,
+    }
+    return {
+        name: "galejs",
+        async load(filepath) {
+            if (filepath.includes("node_modules") === true) {
+                return null
+            }
+            const source = fs.read(filepath)
+            const code = await transpile({
+                filename: filepath,
+                source,
+                sourceImport: opts.sourceImport
+            })
+            return code
         }
-        const source = fs.read(filepath)
-        const code = await transpile(source)
-        return code
     }
 }
